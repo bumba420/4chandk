15a3066929a8d5cb8ba6122d7950554256d0c4da
bottiger <bottiger@fe922574-e228-0410-9b08-7b37ea817361>
2007-02-15 17:08:56 +0000

    ""

--- languages/english.php
+++ languages/english.php
@@ -1,2 +1,10 @@
 <?php
+// The form
+Language::set('form:name', 		'Name');
+Language::set('form:email', 	'E-mail');
+Language::set('form:subject',	'Subject');
+Language::set('form:message',	'Comment');
+Language::set('form:file',		'File');
+Language::set('form:password',	'Password');
+Language::set('form:submit',	'Submit');
 ?>
