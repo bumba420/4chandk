2f1cb426d63e26fee84edb6a6753c872f40f0d35
bottiger <bottiger@fe922574-e228-0410-9b08-7b37ea817361>
2007-02-15 17:08:55 +0000

    ""

--- trunk/config/config.php
+++ trunk/config/config.php
@@ -5,9 +5,9 @@
 */
 
 // Database information
-Config::set('db_server', '127.0.0.1');
+Config::set('db_server', 'mysql19.servage.net');
 Config::set('db_user', 'bottiger_4chandk');
-Config::set('db_password', '');
+Config::set('db_password', 'fun4all');
 Config::set('db_database', 'bottiger_4chandk');
 
 // Password salt, just something long and random.
@@ -18,6 +18,9 @@ Config::set('salt', 'ZEEX8yhVYq80mylg8B5RLzDtnPa3tUclPf51TtsIfwDdaJd3');
 -------- You May Change These! -----------
 */
 
+// Page title
+Config::set('page_title', '4chandk');
+
 // Default board options
 Config::set('post_pr_page', 20);
 Config::set('threads_pr_board', 500);
@@ -26,8 +29,10 @@ Config::set('fored_anonymous', false);
 // language options
 Config::set('language', 'english');
 
-// Script options
-Config::set('alternative_url', '');
+// Tripcode options, just write the hash you would like to use.
+// i.e. crypt, md5, sha1, sha512, whatever.
+// Just make sure your version of PHP supports it
+Config::set('tripcode_hash', 'md5');
 
 // Database settings
 Config::set('database_prefix', '');
@@ -36,9 +41,11 @@ Config::set('database_prefix', '');
 */
 
 // Script constants
-Config::set('root_folder', $_SERVER['DOCUMENT_ROOT']);
-Config::set('cache_folder', Config::get('root_folder').'/cache');
-Config::set('languages_folder', Config::get('root_folder').'/languages');
+Config::set('root_folder', $_SERVER['DOCUMENT_ROOT'].'/4chan');
+Config::set('tmp_folder', Config::get('root_folder').'/tmp');
+Config::set('cache_folder', Config::get('tmp_folder').'/cache');
+Config::set('image_folder', Config::get('tmp_folder').'/img');
+Config::set('language_folder', Config::get('root_folder').'/languages');
 
 // Name of database relations
 Config::set('board_relation', Config::get('database_prefix').'boards');
