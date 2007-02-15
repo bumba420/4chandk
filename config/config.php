1d583ea14cc1cda1bc7dec3778234ebeebd97ac8
bottiger <bottiger@fe922574-e228-0410-9b08-7b37ea817361>
2007-02-15 17:09:56 +0000

    ""

--- config/config.php
+++ config/config.php
@@ -5,9 +5,9 @@
 */
 
 // Database information
-Config::set('db_server', 'mysql19.servage.net');
+Config::set('db_server', '127.0.0.1');
 Config::set('db_user', 'bottiger_4chandk');
-Config::set('db_password', 'fun4all');
+Config::set('db_password', '');
 Config::set('db_database', 'bottiger_4chandk');
 
 // Password salt, just something long and random.
